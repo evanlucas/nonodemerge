b81a8c2d8f52f4e9f72ac725e05686c739b88177
Evan Lucas <evanlucas@me.com>
2016-02-28 07:07:08 -0600

    Make commit title red if > 50 columns

--- content_dist.js
+++ content_dist.js
@@ -3,12 +3,31 @@
 'use strict'
 
 var injection = require('github-injection')
+
 injection(global, function() {
   var ele = document.querySelector('.js-merge-branch-action')
   if (ele) {
     ele.classList.add('disabled')
     ele.setAttribute('disabled', 'disabled')
     ele.style.display = 'none'
+  }
+
+  var lis = document.querySelectorAll('li.commit')
+
+  if (!lis || !lis.length) return
+
+  for (var i = 0; i < lis.length; i++) {
+    var li = lis[i]
+
+    var tlCell = li.children[1]
+
+    var a = tlCell.children[0].children[0]
+
+    var title = a.innerText
+
+    if (title.length > 50) {
+      a.style.color = '#db2c38'
+    }
   }
 })
 
